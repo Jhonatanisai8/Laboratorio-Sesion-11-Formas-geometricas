e792f0b5b5c73cf6da555f5f864c1d4b3aea1bef
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-30 11:44:09 -0500

    clase manejo-archivos: metodo para guardar un archivo

--- src/main/java/com/jhonatan/app/Archivos/ManejoArchivos.java
+++ src/main/java/com/jhonatan/app/Archivos/ManejoArchivos.java
@@ -1,10 +1,26 @@
 package com.jhonatan.app.Archivos;
 
+import com.jhonatan.app.Arboles.Arbol;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 
 /**
  *
  * @author Jhonatan
  */
 public class ManejoArchivos {
-    
+    public void guardar(JFrame panel,Arbol a){
+        javax.swing.filechooser.FileFilter filtro = new FileNameExtensionFilter("Arhivo de arboles (.tree)", "tree");
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(filtro);
+        
+        int seleccion = fileChooser.showSaveDialog(panel);
+        if (seleccion == JFileChooser.APPROVE_OPTION) {
+            File fichero = fileChooser.getSelectedFile();
+            guardarEnArchivo(fichero.getAbsolutePath());
+        }
+    }
 }
