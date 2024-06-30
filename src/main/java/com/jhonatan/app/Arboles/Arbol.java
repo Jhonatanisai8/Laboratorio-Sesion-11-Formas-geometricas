e1ec6521c2cc5559fce3b8421d4b71eb1e88691d
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-30 16:00:27 -0500

    clase arbol: atributos, getters y setters

--- src/main/java/com/jhonatan/app/Arboles/Arbol.java
+++ src/main/java/com/jhonatan/app/Arboles/Arbol.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.jhonatan.app.Arboles;
+
+import java.util.LinkedList;
 
 /**
  *
  * @author Jhonatan
  */
 public class Arbol {
-    
+
+    public Nodo cabeza = null;
+    public int TAM;
+    public LinkedList lineas = new LinkedList();
+    public LinkedList circulos = new LinkedList();
+
+    public Nodo getCabeza() {
+        return cabeza;
+    }
+
+    public void setCabeza(Nodo cabeza) {
+        this.cabeza = cabeza;
+    }
+
+    public int getTAM() {
+        return TAM;
+    }
+
+    public void setTAM(int TAM) {
+        this.TAM = TAM;
+    }
+
+    public LinkedList getLineas() {
+        return lineas;
+    }
+
+    public void setLineas(LinkedList lineas) {
+        this.lineas = lineas;
+    }
+
+    public LinkedList getCirculos() {
+        return circulos;
+    }
+
+    public void setCirculos(LinkedList circulos) {
+        this.circulos = circulos;
+    }
+
 }
