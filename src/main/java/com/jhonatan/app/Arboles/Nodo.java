3395f5ff55ffa6835cff6a1bd0d9a40af49d5bda
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-30 15:41:40 -0500

    clase nodo: atributos y constructores

--- src/main/java/com/jhonatan/app/Arboles/Nodo.java
+++ src/main/java/com/jhonatan/app/Arboles/Nodo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.jhonatan.app.Arboles;
 
 /**
@@ -9,5 +5,20 @@ package com.jhonatan.app.Arboles;
  * @author Jhonatan
  */
 public class Nodo {
-    
+
+    private Nodo padre = null;
+    private Nodo izq = null, dere = null;
+    private Object valor;
+
+    //esto es solo algo que uso para podernlos imprimir graficamente
+    private int nivel;
+
+    public Nodo(Nodo padre, Object valor) {
+        this.padre = padre;
+        this.valor = valor;
+    }
+
+    public Nodo(Object valor) {
+        this.valor = valor;
+    }
 }
