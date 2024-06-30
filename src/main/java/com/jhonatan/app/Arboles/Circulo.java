21727463b890b772b65d4acf481ca689a5a4dc51
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-30 15:52:38 -0500

    clase circulo: atributos y constructores

--- src/main/java/com/jhonatan/app/Arboles/Circulo.java
+++ src/main/java/com/jhonatan/app/Arboles/Circulo.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.jhonatan.app.Arboles;
+
+import java.awt.Color;
 
 /**
  *
  * @author Jhonatan
  */
 public class Circulo {
-    
+
+    public Color borde = Color.BLACK;
+    public Color relleno = Color.BLACK;
+    public int tam = 0;
+    public int com, x, y;
+    public Nodo nodo;
+
+    public Circulo() {
+    }
+
+    public Circulo(Object com, int x, int y) {
+        this.com = Integer.parseInt(String.valueOf(com));
+        this.x = x;
+        this.y = y;
+    }
+
 }
